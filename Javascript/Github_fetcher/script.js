2348f1ac538cfff65c7d5192fd7090221f1d5888
mashhoor-ahdal <mashhoorahdal2@gmail.com>
2024-01-14 12:26:25 +0530

    Some debugs

--- Javascript/Github_fetcher/script.js
+++ Javascript/Github_fetcher/script.js
@@ -1,48 +1,39 @@
+const form = document.getElementById('input_form');
+const resultDiv = document.getElementById('result');
+resultDiv.style.display = "none";
 
-var form =document.getElementById('input_form')
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-const resultDiv=document.getElementById('result')
-resultDiv.style.display="none"
+    const userNameInput = document.getElementById('username');
+    const userName = userNameInput.value;
 
+    const resultImg = resultDiv.querySelector('a > img');
+    const resultAnchor = resultDiv.querySelector('a');
+    const firstPara = resultDiv.querySelector('p');
 
+    const para = document.createElement('p');
+    const anchor = document.createElement('a');
 
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-    let userName=document.getElementById('username').value
-    const resultImg=resultDiv.querySelector('a > img')
-    const resultAnch=resultDiv.querySelector('a')
-    const firstPara=resultDiv.querySelector('p')
-    const para=document.createElement('p')
-    const achor=document.createElement('a')
-     
-    fetch("https://api.github.com/users/"+userName).then(response=>response.json().then(data=>{
-        console.log(data)
-        resultDiv.style.display="block"
-        resultAnch.href=data.html_url
-        resultImg.src=data.avatar_url
-        para.textContent="Github : "
-        para.className="github"
-        achor.className="githubUrl"
-        achor.href=data.url
-        achor.textContent="Visit"
-        para.appendChild(achor)
-        resultDiv.appendChild(para)
-        if (data.name){
-        firstPara.textContent+=data.name  
-        }
-        else {
-        firstPara.textContent+=data.login  
-        }
-        
-        
+    fetch(`https://api.github.com/users/${userName}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            resultDiv.style.display = "block";
 
-        
-        // firstPara=
-        
-         
-    }));
-   
-    
+            resultDiv.innerHTML="";
 
-})
+            resultAnchor.href = data.html_url;
+            resultImg.src = data.avatar_url;
+            para.textContent = "Github : ";
+            para.className = "github";
+            anchor.className = "githubUrl";
+            anchor.href = data.url;
+            anchor.textContent = "Visit";
 
+            para.appendChild(anchor);
+            resultDiv.appendChild(para);
+
+            firstPara.textContent = data.name ?"Name:"+ data.name :"Name: "+ data.login;
+        });
+});
